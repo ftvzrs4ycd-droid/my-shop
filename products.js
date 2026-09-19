12adcc57c92b2b3742e93428f4deea8d9a768381
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-19 20:09:19 +0800

    Update products.js

--- products.js
+++ products.js
@@ -49,10 +49,10 @@ const SUB_CATEGORIES = [
 // 商品列表
 // ============================================
 const PRODUCTS = [
-  { id: 1, catId: 1, name: 'MM 有机胡萝卜 1kg', img: 'https://images.unsplash.com/photo-1598170845058-32b9d6a5da37?w=400', price: 29.9, oldPrice: 35, sold: 16, stock: 5, isNew: true },
-  { id: 2, catId: 2, name: '朝日唯品 百香果青瓜风味', img: 'https://images.unsplash.com/photo-1550583724-b2692b85b150?w=400', price: 22.8, oldPrice: 28, sold: 27, stock: 10, isNew: true },
-  { id: 3, catId: 1, name: 'MM 椰奶布丁酥皮挞', img: 'https://images.unsplash.com/photo-1551024506-0bccd828d307?w=400', price: 19.9, oldPrice: 25, sold: 22, stock: 8, isNew: true },
-  { id: 4, catId: 3, name: 'MM 麻薯', img: 'https://images.unsplash.com/photo-1587241321921-91a834d6d191?w=400', price: 15, oldPrice: 18, sold: 6, stock: 20 },
+  { id: 1, catId: 1, name: 'MM 有机胡萝卜 1kg', img: 'https://github.com/ftvzrs4ycd-droid/my-shop/blob/main/images/5fe317698fb16276c5128263cb639a75.jpg?raw=true', price: 29.9, oldPrice: 35, sold: 16, stock: 5, isNew: true },
+  { id: 2, catId: 2, name: '朝日唯品 百香果青瓜风味', img: 'https://github.com/ftvzrs4ycd-droid/my-shop/blob/main/images/3f501ac07b40662bc7606e599dcaab80.jpg?raw=true', price: 22.8, oldPrice: 28, sold: 27, stock: 10, isNew: true },
+  { id: 3, catId: 1, name: 'MM 椰奶布丁酥皮挞', img: 'https://github.com/ftvzrs4ycd-droid/my-shop/blob/main/images/c218cfd5355de0d5b40aa422706cc137.jpg?raw=true', price: 19.9, oldPrice: 25, sold: 22, stock: 8, isNew: true },
+  { id: 4, catId: 3, name: 'MM 麻薯', img: 'https://github.com/ftvzrs4ycd-droid/my-shop/blob/main/images/debc6fc3e90462738afe2ed17134852c.jpg?raw=true', price: 15, oldPrice: 18, sold: 6, stock: 20 },
   { id: 5, catId: 8, name: 'MM 精选鲜鸡蛋 1.59kg', img: 'https://images.unsplash.com/photo-1587486913049-af64b90cfb2e?w=400', price: 31.9, oldPrice: 38, sold: 8, stock: 15 },
   { id: 6, catId: 1, name: 'MM 新款原味伯爵瑞士卷', img: 'https://images.unsplash.com/photo-1587314168485-3236d6710814?w=400', price: 26, oldPrice: 32, sold: 15, stock: 6 },
   { id: 7, catId: 1, name: 'MM 榴芒班戟', img: 'https://images.unsplash.com/photo-1563729768-397acd398e13?w=400', price: 19.9, oldPrice: 24, sold: 12, stock: 4 },
