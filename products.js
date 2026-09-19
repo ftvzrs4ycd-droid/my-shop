cdb7db06f0e8f1f95188d47f114dc9493ec8139d
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-19 19:29:12 +0800

    Add new product with id 15 to products list

--- products.js
+++ products.js
@@ -75,4 +75,5 @@ const PRODUCTS = [
   { id: 12, catId: 1, name: '墨江紫米吐司面包 500g', img: 'https://images.unsplash.com/photo-1509440159596-0249088772ff?w=400', price: 39.9, oldPrice: 45, sold: 11, stock: 9 },
   { id: 13, catId: 1, name: 'MM 有机胡萝卜 1kg', img: '...', price: 29.9, oldPrice: 35, sold: 16, stock: 5, isNew: true },
   { id: 14, catId: 1, name: '888888 500g', img: '...', price: 39.9, oldPrice: 45, sold: 11, stock: 9 },
+  { id: 15, catId: 1, name: '999999 500g', img: '...', price: 39.9, oldPrice: 45, sold: 11, stock: 9 },
 ];
