dcf2c7c1450e029a67625caabbe1567fc42f8ed5
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-21 17:03:17 +0800

    Update products-2rou.js

--- products-2rou.js
+++ products-2rou.js
@@ -1,4 +1,4 @@
 // 肉蛋果蔬 (catId: 1)
 window.PRODUCTS_CAT_1 = [
-  { id: 1, catId: 1, subCatId: 103, name: '456456', img: '456', price: 50, oldPrice: 50, sold: 0, stock: 100, isNew: true },
+  { id: 1, catId: 1, subCatId: 103, name: '45646', img: '456456', price: 50, oldPrice: 50, sold: 0, stock: 100, isNew: true },
 ];
