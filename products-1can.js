a3c690e71833cbba8f034fae2674aa9907bbc6a8
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-21 22:14:41 +0800

    Update products-1can.js

--- products-1can.js
+++ products-1can.js
@@ -1,7 +1,6 @@
 // 餐吧 (catId: 8)
 window.PRODUCTS_CAT_8 = [
   { id: 1, catId: 8, subCatId: 802, name: 'MM炸鸡桶', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/1can/1.png?raw=true', price: 35, oldPrice: 35, sold: 0, stock: 10000, isNew: true },
-  { id: 2, catId: 8, subCatId: 802, name: 'MM咸蛋黄小龙虾披萨1/6.', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/1can/2.png?raw=true', price: 13.9, oldPrice: 13.9, sold: 0, stock: 10000, isNew: true },
   { id: 3, catId: 8, subCatId: 802, name: 'MM烤牛肉卷', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/1can/3.png?raw=true', price: 20, oldPrice: 20, sold: 0, stock: 10000, isNew: true },
   { id: 4, catId: 8, subCatId: 802, name: 'MM泡菜猪梅肉卷', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/1can/4.png?raw=true', price: 18, oldPrice: 18, sold: 0, stock: 10000, isNew: true },
   { id: 5, catId: 8, subCatId: 802, name: 'MM 黄金厚虾堡', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/1can/5.png?raw=true', price: 20, oldPrice: 20, sold: 0, stock: 10000, isNew: true },
@@ -10,4 +9,6 @@ window.PRODUCTS_CAT_8 = [
   { id: 8, catId: 8, subCatId: 802, name: 'MM咸蛋黄小龙虾披萨', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/1can/8.png?raw=true', price: 69.9, oldPrice: 69.9, sold: 0, stock: 10000, isNew: true },
   { id: 9, catId: 8, subCatId: 802, name: 'MM咸蛋黄小龙虾披萨', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/1can/9.png?raw=true', price: 13.9, oldPrice: 13.9, sold: 0, stock: 10000, isNew: true },
   { id: 10, catId: 8, subCatId: 802, name: 'MM至尊披萨1/6', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/1can/10.png?raw=true', price: 13, oldPrice: 13, sold: 0, stock: 100, isNew: true },
+  { id: 11, catId: 8, subCatId: 804, name: 'MM苏丹王榴莲千层.', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/1can/2.png?raw=true', price: 25, oldPrice: 25, sold: 0, stock: 10000, isNew: true },
+  { id: 12, catId: 8, subCatId: 803, name: '凯撒沙拉.', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/1can/11.png?raw=true', price: 15.9, oldPrice: 15.9, sold: 0, stock: 10000, isNew: true },
 ];
