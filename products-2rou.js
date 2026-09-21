926f89375cd02ff0ce0d76fd79f656d5200ba0f8
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-21 22:50:05 +0800

    Update products-2rou.js

--- products-2rou.js
+++ products-2rou.js
@@ -1,4 +1,9 @@
 // 肉蛋果蔬 (catId: 1)
 window.PRODUCTS_CAT_1 = [
-  { id: 1, catId: 1, subCatId: 103, name: '45646', img: '456456', price: 50, oldPrice: 50, sold: 0, stock: 100, isNew: true },
+  { id: 1, catId: 1, subCatId: 102, name: '怡颗莓云南超大蓝莓750g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/2rou/1sg/1.png?raw=true', price: 108.8, oldPrice: 108.8, sold: 0, stock: 10000, isNew: true },
+  { id: 2, catId: 1, subCatId: 102, name: '福安巨峰葡萄2.4kg', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/2rou/1sg/2.png?raw=true', price: 79.9, oldPrice: 79.9, sold: 0, stock: 10000, isNew: true },
+  { id: 3, catId: 1, subCatId: 102, name: '佳沛阳光金果超大果12粒礼盒', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/2rou/1sg/3.png?raw=true', price: 139.9, oldPrice: 139.9, sold: 0, stock: 10000, isNew: true },
+  { id: 4, catId: 1, subCatId: 102, name: '恐龙蛋李1.5kg', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/2rou/1sg/4.png?raw=true', price: 69.9, oldPrice: 69.9, sold: 0, stock: 10000, isNew: true },
+  { id: 5, catId: 1, subCatId: 102, name: '澳洲2PH柑 3kg/箱', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/2rou/1sg/5.png?raw=true', price: 89.9, oldPrice: 89.9, sold: 0, stock: 10000, isNew: true },
+  { id: 6, catId: 1, subCatId: 102, name: '(Zespri) 22#佳沛阳光金果 3kg', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/2rou/1sg/6.png?raw=true', price: 189, oldPrice: 189, sold: 0, stock: 10000, isNew: true },
 ];
