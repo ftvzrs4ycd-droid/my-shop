6d9dc4b3967fa5bbcc0e79ffe2177962be89e825
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-21 19:18:35 +0800

    Update products-1can.js

--- products-1can.js
+++ products-1can.js
@@ -3,4 +3,6 @@ window.PRODUCTS_CAT_8 = [
   { id: 1, catId: 8, subCatId: 802, name: 'MM炸鸡桶', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/1can/1.png?raw=true', price: 35, oldPrice: 35, sold: 0, stock: 10000, isNew: true },
   { id: 2, catId: 8, subCatId: 802, name: 'MM咸蛋黄小龙虾披萨1/6.', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/1can/2.png?raw=true', price: 13.9, oldPrice: 13.9, sold: 0, stock: 10000, isNew: true },
   { id: 3, catId: 8, subCatId: 802, name: 'MM烤牛肉卷', img: 'https://github.com/ftvzrs4ycd-droid/my-shop/blob/main/images/1can/3.png?raw=true', price: 20, oldPrice: 20, sold: 0, stock: 10000, isNew: true },
+  { id: 4, catId: 8, subCatId: 802, name: 'MM泡菜猪梅肉卷', img: 'https://github.com/ftvzrs4ycd-droid/my-shop/blob/main/images/1can/4.png?raw=true', price: 18, oldPrice: 18, sold: 0, stock: 10000, isNew: true },
+  { id: 5, catId: 8, subCatId: 802, name: 'MM 黄金厚虾堡', img: 'https://github.com/ftvzrs4ycd-droid/my-shop/blob/main/images/1can/5.png?raw=true', price: 20, oldPrice: 20, sold: 0, stock: 10000, isNew: true },
 ];
