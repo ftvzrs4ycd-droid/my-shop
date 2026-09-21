40c7f23851d9bc4ec6538a34adb73e261f63ed9e
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-21 18:10:58 +0800

    Update products-1can.js

--- products-1can.js
+++ products-1can.js
@@ -1,1 +1,5 @@
 
+// 餐吧 (catId: 8)
+window.PRODUCTS_CAT_8 = [
+  { id: 1, catId: 8, subCatId: 802, name: 'MM炸鸡桶', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/1can/1.png?raw=true', price: 35, oldPrice: 35, sold: 0, stock: 10000, isNew: true },
+];
