8cf6f043556f8983be548a16d88a82bf083038da
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-21 18:17:12 +0800

    Update products-1can.js

--- products-1can.js
+++ products-1can.js
@@ -1,5 +1,5 @@
-
 // 餐吧 (catId: 8)
 window.PRODUCTS_CAT_8 = [
   { id: 1, catId: 8, subCatId: 802, name: 'MM炸鸡桶', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/1can/1.png?raw=true', price: 35, oldPrice: 35, sold: 0, stock: 10000, isNew: true },
+  { id: 2, catId: 8, subCatId: 802, name: 'MM咸蛋黄小龙虾披萨1/6.', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/1can/2.png?raw=true', price: 13.9, oldPrice: 13.9, sold: 0, stock: 10000, isNew: true },
 ];
