1632b20eaed1ca5c1303e79697b40638a42a62d0
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-22 20:55:36 +0800

    Update products-3ru.js

--- products-3ru.js
+++ products-3ru.js
@@ -6,4 +6,7 @@ window.PRODUCTS_CAT_2 = [
   { id: 4, catId: 2, subCatId: 203, name: 'Member\'s Mark 原味伯爵瑞士卷 1*12\'s', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/2dg/1.png?raw=true', price: 45, oldPrice: 45, sold: 0, stock: 100, isNew: true },
   { id: 5, catId: 2, subCatId: 203, name: 'Member\'s Mark 榴莲大福 1*16\'S', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/2dg/2.png?raw=true', price: 79.9, oldPrice: 79.9, sold: 0, stock: 100, isNew: true },
   { id: 6, catId: 2, subCatId: 203, name: 'Member\'s Mark 意式提拉米苏 9*100g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/2dg/3.png?raw=true', price: 59.9, oldPrice: 59.9, sold: 0, stock: 100, isNew: true },
+  { id: 7, catId: 2, subCatId: 204, name: 'Member\'s Mark 低脂高钙鲜牛奶 2L', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/3xnxk/1.png?raw=true', price: 24.9, oldPrice: 24.9, sold: 0, stock: 100, isNew: true },
+  { id: 8, catId: 2, subCatId: 204, name: '鲜牛奶 全脂巴氏杀菌乳 2L', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/3xnxk/2.png?raw=true', price: 17.9, oldPrice: 17.9, sold: 0, stock: 100, isNew: true },
+  { id: 9, catId: 2, subCatId: 204, name: 'Member\'s Mark 有机高钙鲜牛奶 240g*8', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/3xnxk/3.png?raw=true', price: 45.9, oldPrice: 45.9, sold: 0, stock: 100, isNew: true },
 ];
