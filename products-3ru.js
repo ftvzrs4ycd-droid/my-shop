c504838df73cd0c994bc8eab1421a82383e91b79
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-22 20:37:49 +0800

    Update products-3ru.js

--- products-3ru.js
+++ products-3ru.js
@@ -1,1 +1,7 @@
 
+// 乳品烘焙 (catId: 2)
+window.PRODUCTS_CAT_2 = [
+  { id: 1, catId: 2, subCatId: 202, name: 'Member\'s Mark 黑猪肉鲜肉月饼 1*12\'S', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/1mbdx/1.png?raw=true', price: 35.9, oldPrice: 35.9, sold: 0, stock: 100, isNew: true },
+  { id: 2, catId: 2, subCatId: 202, name: '多乐之日 生吐司面包 880g(5袋)', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/1mbdx/2.png?raw=true', price: 25.9, oldPrice: 25.9, sold: 0, stock: 100, isNew: true },
+  { id: 3, catId: 2, subCatId: 202, name: '高蛋白黄油餐包 720g(6包)', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/1mbdx/3.png?raw=true', price: 29.9, oldPrice: 29.9, sold: 0, stock: 100, isNew: true },
+];
