ed798eeb75d68a15d1564164e392ae11a18bce03
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-22 20:46:33 +0800

    Update products-3ru.js

--- products-3ru.js
+++ products-3ru.js
@@ -1,7 +1,9 @@
-
 // 乳品烘焙 (catId: 2)
 window.PRODUCTS_CAT_2 = [
   { id: 1, catId: 2, subCatId: 202, name: 'Member\'s Mark 黑猪肉鲜肉月饼 1*12\'S', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/1mbdx/1.png?raw=true', price: 35.9, oldPrice: 35.9, sold: 0, stock: 100, isNew: true },
   { id: 2, catId: 2, subCatId: 202, name: '多乐之日 生吐司面包 880g(5袋)', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/1mbdx/2.png?raw=true', price: 25.9, oldPrice: 25.9, sold: 0, stock: 100, isNew: true },
   { id: 3, catId: 2, subCatId: 202, name: '高蛋白黄油餐包 720g(6包)', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/1mbdx/3.png?raw=true', price: 29.9, oldPrice: 29.9, sold: 0, stock: 100, isNew: true },
+  { id: 4, catId: 2, subCatId: 203, name: 'Member\'s Mark 原味伯爵瑞士卷 1*12\'s', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/2dg/1.png?raw=true', price: 45, oldPrice: 45, sold: 0, stock: 100, isNew: true },
+  { id: 5, catId: 2, subCatId: 203, name: 'Member\'s Mark 榴莲大福 1*16\'S', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/2dg/2.png?raw=true', price: 79.9, oldPrice: 79.9, sold: 0, stock: 100, isNew: true },
+  { id: 6, catId: 2, subCatId: 203, name: 'Member\'s Mark 意式提拉米苏 9*100g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/2dg/3.png?raw=true', price: 59.9, oldPrice: 59.9, sold: 0, stock: 100, isNew: true },
 ];
