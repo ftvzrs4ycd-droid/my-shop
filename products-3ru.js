e0e4faa858718b454c5f39fb4b22390cafe7f3c4
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-23 15:37:59 +0800

    Update products-3ru.js

--- products-3ru.js
+++ products-3ru.js
@@ -24,4 +24,7 @@ window.PRODUCTS_CAT_2 = [
   { id: 22, catId: 2, subCatId: 209, name: 'Member\'s Mark 高纤快熟燕麦片 3kg', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/8gwct/1.png?raw=true', price: 49.9, oldPrice: 49.9, sold: 0, stock: 100, isNew: true },
   { id: 23, catId: 2, subCatId: 209, name: 'Member\'s Mark 黑芝麻谷物粉 1.28kg', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/8gwct/2.png?raw=true', price: 79.9, oldPrice: 79.9, sold: 0, stock: 100, isNew: true },
   { id: 24, catId: 2, subCatId: 209, name: '桂格 A2β-酪蛋白牛乳蒸糯燕麦片 40g*30', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/8gwct/3.png?raw=true', price: 59.9, oldPrice: 59.9, sold: 0, stock: 100, isNew: true },
+  { id: 25, catId: 2, subCatId: 210, name: '吉士汀 缤纷奶酪棒 600g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/9hynl/1.png?raw=true', price: 59.9, oldPrice: 59.9, sold: 0, stock: 100, isNew: true },
+  { id: 26, catId: 2, subCatId: 210, name: 'VEPO CHEESE 荷兰进口 奶酪工坊组合干酪片(高达干酪片200g+伊顿干酪片200g)', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/9hynl/2.png?raw=true', price: 45.9, oldPrice: 45.9, sold: 0, stock: 100, isNew: true },
+  { id: 27, catId: 2, subCatId: 210, name: '美兰 新西兰进口 原味黄油 500g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/3ru/9hynl/3.png?raw=true', price: 46.9, oldPrice: 46.9, sold: 0, stock: 100, isNew: true },
 ];
