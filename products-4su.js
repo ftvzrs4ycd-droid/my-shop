0ee14e23d695e250b37dba7e8ddeaaecc3a600cd
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-23 17:20:15 +0800

    Update products-4su.js

--- products-4su.js
+++ products-4su.js
@@ -1,7 +1,9 @@
-
 // 速食冷冻 (catId: 3)
 window.PRODUCTS_CAT_3 = [
   { id: 1, catId: 3, subCatId: 302, name: 'Member\'s Mark 番茄肉酱意面 1.128千克', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/4su/1ksc/1.png?raw=true', price: 39.9, oldPrice: 39.9, sold: 0, stock: 100, isNew: true },
   { id: 2, catId: 3, subCatId: 302, name: 'Swift 芝士牛肉卷 840g(280g*3)', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/4su/1ksc/2.png?raw=true', price: 65.9, oldPrice: 65.9, sold: 0, stock: 100, isNew: true },
   { id: 3, catId: 3, subCatId: 302, name: '米良人良 贵州红酸汤米粉 1.836kg', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/4su/1ksc/3.png?raw=true', price: 59.9, oldPrice: 59.9, sold: 0, stock: 100, isNew: true },
+  { id: 4, catId: 3, subCatId: 303, name: '泰森 奥尔良风味鸡翅中 1.221kg', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/4su/2ssrzp/1.png?raw=true', price: 69.9, oldPrice: 69.9, sold: 0, stock: 100, isNew: true },
+  { id: 5, catId: 3, subCatId: 303, name: '泰森(Tyson) 鲜嫩脆皮鸡块 2kg', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/4su/2ssrzp/2.png?raw=true', price: 47.9, oldPrice: 47.9, sold: 0, stock: 100, isNew: true },
+  { id: 6, catId: 3, subCatId: 303, name: '牙签羔羊肉 420g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/4su/2ssrzp/3.png?raw=true', price: 79.9, oldPrice: 79.9, sold: 0, stock: 100, isNew: true },
 ];
