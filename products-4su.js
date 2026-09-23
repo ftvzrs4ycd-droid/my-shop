f95a96166151712358f20fad9431bd14a54322da
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-23 17:12:00 +0800

    Update products-4su.js

--- products-4su.js
+++ products-4su.js
@@ -1,1 +1,7 @@
 
+// 速食冷冻 (catId: 3)
+window.PRODUCTS_CAT_3 = [
+  { id: 1, catId: 3, subCatId: 302, name: 'Member\'s Mark 番茄肉酱意面 1.128千克', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/4su/1ksc/1.png?raw=true', price: 39.9, oldPrice: 39.9, sold: 0, stock: 100, isNew: true },
+  { id: 2, catId: 3, subCatId: 302, name: 'Swift 芝士牛肉卷 840g(280g*3)', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/4su/1ksc/2.png?raw=true', price: 65.9, oldPrice: 65.9, sold: 0, stock: 100, isNew: true },
+  { id: 3, catId: 3, subCatId: 302, name: '米良人良 贵州红酸汤米粉 1.836kg', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/4su/1ksc/3.png?raw=true', price: 59.9, oldPrice: 59.9, sold: 0, stock: 100, isNew: true },
+];
