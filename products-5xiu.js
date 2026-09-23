a139ef7d7c7d0b990750494ecdfccf74c67a64b7
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-23 20:20:17 +0800

    Update products-5xiu.js

--- products-5xiu.js
+++ products-5xiu.js
@@ -1,7 +1,9 @@
-
 // 休闲零食 (catId: 4)
 window.PRODUCTS_CAT_4 = [
   { id: 1, catId: 4, subCatId: 404, name: 'Member\'s Mark 每日坚果 900g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/5xiu/1dt/1.png?raw=true', price: 99, oldPrice: 99, sold: 0, stock: 100, isNew: true },
   { id: 2, catId: 4, subCatId: 404, name: 'NUTHING 开心果裹腰果仁550g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/5xiu/1dt/2.png?raw=true', price: 79.9, oldPrice: 79.9, sold: 0, stock: 100, isNew: true },
   { id: 3, catId: 4, subCatId: 404, name: 'HRYOUP 苹果干 560g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/5xiu/1dt/3.png?raw=true', price: 56.9, oldPrice: 56.9, sold: 0, stock: 100, isNew: true },
+  { id: 4, catId: 4, subCatId: 405, name: '低脂高蛋白爆汁 鸡胸肉肠 1.1kg', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/5xiu/2rght/1.png?raw=true', price: 66.9, oldPrice: 66.9, sold: 0, stock: 100, isNew: true },
+  { id: 5, catId: 4, subCatId: 405, name: '有友 脱骨鸭掌（山椒味）600g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/5xiu/2rght/2.png?raw=true', price: 69.9, oldPrice: 69.9, sold: 0, stock: 100, isNew: true },
+  { id: 6, catId: 4, subCatId: 405, name: '卤汁牛腱（原味卤香） 450克', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/5xiu/2rght/3.png?raw=true', price: 99.9, oldPrice: 99.9, sold: 0, stock: 100, isNew: true },
 ];
