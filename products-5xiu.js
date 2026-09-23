58d81dc8738d9317c0c2e897148a789dbed1b382
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-23 20:06:47 +0800

    Update products-5xiu.js

--- products-5xiu.js
+++ products-5xiu.js
@@ -1,7 +1,7 @@
 
 // 休闲零食 (catId: 4)
 window.PRODUCTS_CAT_4 = [
-  { id: 1, catId: 4, subCatId: 402, name: 'Member\'s Mark 每日坚果 900g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/5xiu/1dt/1.png?raw=true', price: 99, oldPrice: 99, sold: 0, stock: 100, isNew: true },
-  { id: 2, catId: 4, subCatId: 402, name: 'NUTHING 开心果裹腰果仁550g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/5xiu/1dt/2.png?raw=true', price: 79.9, oldPrice: 79.9, sold: 0, stock: 100, isNew: true },
-  { id: 3, catId: 4, subCatId: 402, name: 'HRYOUP 苹果干 560g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/5xiu/1dt/3.png?raw=true', price: 56.9, oldPrice: 56.9, sold: 0, stock: 100, isNew: true },
+  { id: 1, catId: 4, subCatId: 404, name: 'Member\'s Mark 每日坚果 900g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/5xiu/1dt/1.png?raw=true', price: 99, oldPrice: 99, sold: 0, stock: 100, isNew: true },
+  { id: 2, catId: 4, subCatId: 404, name: 'NUTHING 开心果裹腰果仁550g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/5xiu/1dt/2.png?raw=true', price: 79.9, oldPrice: 79.9, sold: 0, stock: 100, isNew: true },
+  { id: 3, catId: 4, subCatId: 404, name: 'HRYOUP 苹果干 560g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/5xiu/1dt/3.png?raw=true', price: 56.9, oldPrice: 56.9, sold: 0, stock: 100, isNew: true },
 ];
