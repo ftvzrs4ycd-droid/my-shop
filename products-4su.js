555d4c4193cd156d69858b767590a65132698b64
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-23 17:41:46 +0800

    Update products-4su.js

--- products-4su.js
+++ products-4su.js
@@ -9,4 +9,7 @@ window.PRODUCTS_CAT_3 = [
   { id: 7, catId: 3, subCatId: 304, name: 'Member\'s Mark 台式香肠 250g*4', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/4su/3xcht/1.png?raw=true', price: 67.9, oldPrice: 67.9, sold: 0, stock: 100, isNew: true },
   { id: 8, catId: 3, subCatId: 304, name: '荷美尔(Hormel) 午餐肉肠 900G(300G*3)', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/4su/3xcht/2.png?raw=true', price: 49.9, oldPrice: 49.9, sold: 0, stock: 100, isNew: true },
   { id: 9, catId: 3, subCatId: 304, name: 'aidells 苹果鸡肉香肠 1千克', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/4su/3xcht/3.png?raw=true', price: 59.9, oldPrice: 59.9, sold: 0, stock: 100, isNew: true },
+  { id: 10, catId: 3, subCatId: 305, name: 'Member\'s Mark 双肉土豆泥沙拉', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/4su/4ss/1.png?raw=true', price: 35.9, oldPrice: 35.9, sold: 0, stock: 100, isNew: true },
+  { id: 11, catId: 3, subCatId: 305, name: 'Member\'s Mark 深海鳕鱼三明治 1X1PC', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/4su/4ss/2.png?raw=true', price: 55.9, oldPrice: 55.9, sold: 0, stock: 100, isNew: true },
+  { id: 12, catId: 3, subCatId: 305, name: '花雕熟醉罗氏虾 800g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/4su/4ss/3.png?raw=true', price: 99.9, oldPrice: 99.9, sold: 0, stock: 100, isNew: true },
 ];
