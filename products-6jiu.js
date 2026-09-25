63d258c39aae8af428221cd53e4f4ad218a7210e
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-25 23:27:05 +0800

    Update products-6jiu.js

--- products-6jiu.js
+++ products-6jiu.js
@@ -1,1 +1,7 @@
 
+// 酒水饮料 (catId: 5)
+window.PRODUCTS_CAT_5 = [
+  { id: 1, catId: 5, subCatId: 502, name: '德式小麦啤酒(鲜啤酒)1L*2', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/6jiu/1pj/1.png?raw=true', price: 39.9, oldPrice: 39.9, sold: 0, stock: 100, isNew: true },
+  { id: 2, catId: 5, subCatId: 502, name: '喜力 啤酒 500mL*10罐', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/6jiu/1pj/2.png?raw=true', price: 59.9, oldPrice: 59.9, sold: 0, stock: 100, isNew: true },
+  { id: 3, catId: 5, subCatId: 502, name: '百威 啤酒 255mL *24*', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/6jiu/1pj/3.png?raw=true', price: 64.9, oldPrice: 64.9, sold: 0, stock: 100, isNew: true },
+];
