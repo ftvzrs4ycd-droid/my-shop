0c2e72a136cd53f805c21f2aba1c885dbf7e9ab6
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-26 15:11:11 +0800

    Update products-8ge.js

--- products-8ge.js
+++ products-8ge.js
@@ -1,7 +1,9 @@
-
 // 个护美妆 (catId: 7)
 window.PRODUCTS_CAT_7 = [
   { id: 1, catId: 7, subCatId: 702, name: '丝塔芙 舒润保湿霜套组453g*2', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/8ge/1qd/1.png?raw=true', price: 216.9, oldPrice: 216.9, sold: 0, stock: 100, isNew: true },
   { id: 2, catId: 7, subCatId: 702, name: 'DHC 日本进口橄榄护唇膏1.5g*4 支', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/8ge/1qd/2.png?raw=true', price: 148.9, oldPrice: 148.9, sold: 0, stock: 100, isNew: true },
   { id: 3, catId: 7, subCatId: 702, name: '碧柔 日本进口 悬挂式身体乳 285mL*2', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/8ge/1qd/3.png?raw=true', price: 89.9, oldPrice: 89.9, sold: 0, stock: 100, isNew: true },
+  { id: 4, catId: 7, subCatId: 703, name: '芙丽芳丝(Freeplus) 日本进口 净润洗面霜 100g*3', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/8ge/2mb/1.png?raw=true', price: 199.9, oldPrice: 199.9, sold: 0, stock: 100, isNew: true },
+  { id: 5, catId: 7, subCatId: 703, name: '爱和纯 韩国进口 第三代臻致舒润 保湿面膜27片*27mL', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/8ge/2mb/2.png?raw=true', price: 148.9, oldPrice: 148.9, sold: 0, stock: 100, isNew: true },
+  { id: 6, catId: 7, subCatId: 703, name: '珊珂(SENKA) 日本进口 泡沫洁面膏（保湿）套装 120g*3', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/8ge/2mb/3.png?raw=true', price: 74.9, oldPrice: 74.9, sold: 0, stock: 100, isNew: true },
 ];
