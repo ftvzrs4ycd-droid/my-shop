9dbf530ab2604ddb2e656ce77b43fab530038b71
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-26 22:06:35 +0800

    Update products-9mu.js

--- products-9mu.js
+++ products-9mu.js
@@ -1,1 +1,7 @@
 
+// 母婴玩具 (catId: 9)
+window.PRODUCTS_CAT_9 = [
+  { id: 1, catId: 9, subCatId: 901, name: '爱他美(Aptamil) 荷兰进口 领熠 婴儿配方奶粉(0-6月龄 1段)800g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/1ye/1.png?raw=true', price: 395, oldPrice: 395, sold: 0, stock: 100, isNew: true },
+  { id: 2, catId: 9, subCatId: 901, name: '爱他美(Aptamil) 荷兰进口 卓傲 较大婴儿配方奶粉(6-12月龄 2段)800g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/1ye/2.png?raw=true', price: 299, oldPrice: 299, sold: 0, stock: 100, isNew: true },
+  { id: 3, catId: 9, subCatId: 901, name: '爱他美(Aptamil) 荷兰进口 领熠 较大婴儿配方奶粉(6-12月龄 2段)800g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/1ye/3.png?raw=true', price: 385, oldPrice: 385, sold: 0, stock: 100, isNew: true },
+];
