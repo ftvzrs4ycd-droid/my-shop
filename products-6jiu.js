c179802a9f466b1a1128e72ddf2bc524f400edf3
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-26 10:50:22 +0800

    Update products-6jiu.js

--- products-6jiu.js
+++ products-6jiu.js
@@ -1,7 +1,9 @@
-
 // 酒水饮料 (catId: 5)
 window.PRODUCTS_CAT_5 = [
   { id: 1, catId: 5, subCatId: 502, name: '德式小麦啤酒(鲜啤酒)1L*2', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/6jiu/1pj/1.png?raw=true', price: 39.9, oldPrice: 39.9, sold: 0, stock: 100, isNew: true },
   { id: 2, catId: 5, subCatId: 502, name: '喜力 啤酒 500mL*10罐', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/6jiu/1pj/2.png?raw=true', price: 59.9, oldPrice: 59.9, sold: 0, stock: 100, isNew: true },
   { id: 3, catId: 5, subCatId: 502, name: '百威 啤酒 255mL *24*', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/6jiu/1pj/3.png?raw=true', price: 64.9, oldPrice: 64.9, sold: 0, stock: 100, isNew: true },
+  { id: 4, catId: 5, subCatId: 503, name: '白鹤 日本进口 桃子配制酒 700mL', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/6jiu/2gj/1.png?raw=true', price: 89.9, oldPrice: 89.9, sold: 0, stock: 100, isNew: true },
+  { id: 5, catId: 5, subCatId: 503, name: '布琅兄弟 起泡酒礼盒750mL+200mL', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/6jiu/2gj/2.png?raw=true', price: 119, oldPrice: 119, sold: 0, stock: 100, isNew: true },
+  { id: 6, catId: 5, subCatId: 503, name: '杨梅莲雾西打（果酒）330mL*8', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/6jiu/2gj/3.png?raw=true', price: 53.9, oldPrice: 53.9, sold: 0, stock: 100, isNew: true },
 ];
