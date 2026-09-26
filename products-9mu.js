3df1f6eef11bfe35c12e2778f442d47689320cfa
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-26 22:23:01 +0800

    Update products-9mu.js

--- products-9mu.js
+++ products-9mu.js
@@ -1,7 +1,9 @@
-
 // 母婴玩具 (catId: 9)
 window.PRODUCTS_CAT_9 = [
   { id: 1, catId: 9, subCatId: 901, name: '爱他美(Aptamil) 荷兰进口 领熠 婴儿配方奶粉(0-6月龄 1段)800g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/1ye/1.png?raw=true', price: 395, oldPrice: 395, sold: 0, stock: 100, isNew: true },
   { id: 2, catId: 9, subCatId: 901, name: '爱他美(Aptamil) 荷兰进口 卓傲 较大婴儿配方奶粉(6-12月龄 2段)800g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/1ye/2.png?raw=true', price: 299, oldPrice: 299, sold: 0, stock: 100, isNew: true },
   { id: 3, catId: 9, subCatId: 901, name: '爱他美(Aptamil) 荷兰进口 领熠 较大婴儿配方奶粉(6-12月龄 2段)800g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/1ye/3.png?raw=true', price: 385, oldPrice: 385, sold: 0, stock: 100, isNew: true },
+  { id: 4, catId: 9, subCatId: 902, name: '丹普斯长高高 丹麦进口 有机儿童 调制乳粉 III 510g(255g*2)', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/2sd/1.png?raw=true', price: 748, oldPrice: 748, sold: 0, stock: 100, isNew: true },
+  { id: 5, catId: 9, subCatId: 902, name: 'A2至初 臻智护 澳大利亚进口 儿童配方 调制乳粉 750g*2', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/2sd/2.png?raw=true', price: 598.9, oldPrice: 598.9, sold: 0, stock: 100, isNew: true },
+  { id: 6, catId: 9, subCatId: 902, name: '爱他美 荷兰进口 卓傲 儿童调制乳粉 800g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/2sd/3.png?raw=true', price: 259, oldPrice: 259, sold: 0, stock: 100, isNew: true },
 ];
