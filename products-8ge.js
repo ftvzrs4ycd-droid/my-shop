491e685d99ba2c9a286ece54e68e210bd9ad40d3
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-26 15:14:47 +0800

    Update products-8ge.js

--- products-8ge.js
+++ products-8ge.js
@@ -6,4 +6,7 @@ window.PRODUCTS_CAT_7 = [
   { id: 4, catId: 7, subCatId: 703, name: '芙丽芳丝(Freeplus) 日本进口 净润洗面霜 100g*3', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/8ge/2mb/1.png?raw=true', price: 199.9, oldPrice: 199.9, sold: 0, stock: 100, isNew: true },
   { id: 5, catId: 7, subCatId: 703, name: '爱和纯 韩国进口 第三代臻致舒润 保湿面膜27片*27mL', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/8ge/2mb/2.png?raw=true', price: 148.9, oldPrice: 148.9, sold: 0, stock: 100, isNew: true },
   { id: 6, catId: 7, subCatId: 703, name: '珊珂(SENKA) 日本进口 泡沫洁面膏（保湿）套装 120g*3', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/8ge/2mb/3.png?raw=true', price: 74.9, oldPrice: 74.9, sold: 0, stock: 100, isNew: true },
+  { id: 7, catId: 7, subCatId: 704, name: '多芬 日本进口 空气蓬松保湿洗发露 480g*3', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/8ge/3xf/1.png?raw=true', price: 119.9, oldPrice: 119.9, sold: 0, stock: 100, isNew: true },
+  { id: 8, catId: 7, subCatId: 704, name: '吕 滋盈韧发香氛洗发水 济州微风 650mL*3', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/8ge/3xf/2.png?raw=true', price: 168.9, oldPrice: 168.9, sold: 0, stock: 100, isNew: true },
+  { id: 9, catId: 7, subCatId: 704, name: '潘婷 3分钟奇迹护发素 多效损伤修护香氛版 茉莉山茶香 160mL*4', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/8ge/3xf/3.png?raw=true', price: 99.9, oldPrice: 99.9, sold: 0, stock: 100, isNew: true },
 ];
