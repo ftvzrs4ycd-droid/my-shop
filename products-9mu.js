4b6909c50071e479bd97afeb06e8dcfea8773321
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-26 22:28:46 +0800

    Update products-9mu.js

--- products-9mu.js
+++ products-9mu.js
@@ -6,4 +6,7 @@ window.PRODUCTS_CAT_9 = [
   { id: 4, catId: 9, subCatId: 902, name: '丹普斯长高高 丹麦进口 有机儿童 调制乳粉 III 510g(255g*2)', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/2sd/1.png?raw=true', price: 748, oldPrice: 748, sold: 0, stock: 100, isNew: true },
   { id: 5, catId: 9, subCatId: 902, name: 'A2至初 臻智护 澳大利亚进口 儿童配方 调制乳粉 750g*2', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/2sd/2.png?raw=true', price: 598.9, oldPrice: 598.9, sold: 0, stock: 100, isNew: true },
   { id: 6, catId: 9, subCatId: 902, name: '爱他美 荷兰进口 卓傲 儿童调制乳粉 800g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/2sd/3.png?raw=true', price: 259, oldPrice: 259, sold: 0, stock: 100, isNew: true },
+  { id: 7, catId: 9, subCatId: 903, name: '雅培全安素 特殊医学用途全营养 配方食品麦香味 2.7kg(900g*3)', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/3ty/1.png?raw=true', price: 782.9, oldPrice: 782.9, sold: 0, stock: 100, isNew: true },
+  { id: 8, catId: 9, subCatId: 903, name: '雀巢Nestle 德国进口 超启能恩特殊医学用途婴儿乳蛋白部分水解配方食品 800g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/3ty/2.png?raw=true', price: 428, oldPrice: 428, sold: 0, stock: 100, isNew: true },
+  { id: 9, catId: 9, subCatId: 903, name: '雀巢 荷兰进口肽敏舒特殊医学 用途婴儿乳蛋白深度水解配方食品 400g*2', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/3ty/3.png?raw=true', price: 652, oldPrice: 652, sold: 0, stock: 100, isNew: true },
 ];
