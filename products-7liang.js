122cfaab2dfc5bcaefd3538f2bdeac2c420edc49
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-26 13:33:45 +0800

    Update products-7liang.js

--- products-7liang.js
+++ products-7liang.js
@@ -6,4 +6,7 @@ window.PRODUCTS_CAT_6 = [
   { id: 4, catId: 6, subCatId: 603, name: '十月稻田 优质东北大米长粒香 5kg*2', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/7liang/2dm/1.png?raw=true', price: 69.8, oldPrice: 69.8, sold: 0, stock: 100, isNew: true },
   { id: 5, catId: 6, subCatId: 603, name: '精选龙口粉丝 1kg', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/7liang/2dm/2.png?raw=true', price: 23.9, oldPrice: 23.9, sold: 0, stock: 100, isNew: true },
   { id: 6, catId: 6, subCatId: 603, name: '黑土小町香米 10kg(5kg×2)', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/7liang/2dm/3.png?raw=true', price: 69.9, oldPrice: 69.9, sold: 0, stock: 100, isNew: true },
+  { id: 7, catId: 6, subCatId: 604, name: '有机三色糙米 2.1kg', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/7liang/3wg/1.png?raw=true', price: 39.8, oldPrice: 39.8, sold: 0, stock: 100, isNew: true },
+  { id: 8, catId: 6, subCatId: 604, name: '有机小米 2.3kg', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/7liang/3wg/2.png?raw=true', price: 42.9, oldPrice: 42.9, sold: 0, stock: 100, isNew: true },
+  { id: 9, catId: 6, subCatId: 604, name: '有机三色藜麦米 2.2kg', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/7liang/3wg/3.png?raw=true', price: 88, oldPrice: 88, sold: 0, stock: 100, isNew: true },
 ];
