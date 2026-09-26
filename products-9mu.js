0b1d522f3af4a72a68ae269c0b2ebe4f87845718
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-26 22:33:31 +0800

    Update products-9mu.js

--- products-9mu.js
+++ products-9mu.js
@@ -9,4 +9,7 @@ window.PRODUCTS_CAT_9 = [
   { id: 7, catId: 9, subCatId: 903, name: '雅培全安素 特殊医学用途全营养 配方食品麦香味 2.7kg(900g*3)', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/3ty/1.png?raw=true', price: 782.9, oldPrice: 782.9, sold: 0, stock: 100, isNew: true },
   { id: 8, catId: 9, subCatId: 903, name: '雀巢Nestle 德国进口 超启能恩特殊医学用途婴儿乳蛋白部分水解配方食品 800g', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/3ty/2.png?raw=true', price: 428, oldPrice: 428, sold: 0, stock: 100, isNew: true },
   { id: 9, catId: 9, subCatId: 903, name: '雀巢 荷兰进口肽敏舒特殊医学 用途婴儿乳蛋白深度水解配方食品 400g*2', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/3ty/3.png?raw=true', price: 652, oldPrice: 652, sold: 0, stock: 100, isNew: true },
+  { id: 10, catId: 9, subCatId: 904, name: '帮宝适 一级帮拉拉裤 XXL56片 +XXXL8片', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/4nk/1.png?raw=true', price: 174.9, oldPrice: 174.9, sold: 0, stock: 100, isNew: true },
+  { id: 11, catId: 9, subCatId: 904, name: '全棉时代 婴儿全棉柔巾630片', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/4nk/2.png?raw=true', price: 89.9, oldPrice: 89.9, sold: 0, stock: 100, isNew: true },
+  { id: 12, catId: 9, subCatId: 904, name: '帮宝适 黑金帮拉拉裤2XL箱装 2XL62片+3XL4片', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/9mu/4nk/3.png?raw=true', price: 267.9, oldPrice: 267.9, sold: 0, stock: 100, isNew: true },
 ];
