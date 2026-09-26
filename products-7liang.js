e7ec15237beb7e7b2075fe67bdded84e6b360dd8
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-26 13:29:46 +0800

    Update products-7liang.js

--- products-7liang.js
+++ products-7liang.js
@@ -1,7 +1,9 @@
-
 // 粮油干货 (catId: 6)
 window.PRODUCTS_CAT_6 = [
   { id: 1, catId: 6, subCatId: 602, name: '树番茄汤底 1.2kg (300g*4)', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/7liang/1yj/1.png?raw=true', price: 49.9, oldPrice: 49.9, sold: 0, stock: 100, isNew: true },
   { id: 2, catId: 6, subCatId: 602, name: '有言有味 酸菜多多酸菜鱼调料 282g*5', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/7liang/1yj/2.png?raw=true', price: 49.9, oldPrice: 49.9, sold: 0, stock: 100, isNew: true },
   { id: 3, catId: 6, subCatId: 602, name: '周黑鸭 经典卤料包 1.62kg', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/7liang/1yj/3.png?raw=true', price: 65.9, oldPrice: 65.9, sold: 0, stock: 100, isNew: true },
+  { id: 4, catId: 6, subCatId: 603, name: '十月稻田 优质东北大米长粒香 5kg*2', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/7liang/2dm/1.png?raw=true', price: 69.8, oldPrice: 69.8, sold: 0, stock: 100, isNew: true },
+  { id: 5, catId: 6, subCatId: 603, name: '精选龙口粉丝 1kg', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/7liang/2dm/2.png?raw=true', price: 23.9, oldPrice: 23.9, sold: 0, stock: 100, isNew: true },
+  { id: 6, catId: 6, subCatId: 603, name: '黑土小町香米 10kg(5kg×2)', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/7liang/2dm/3.png?raw=true', price: 69.9, oldPrice: 69.9, sold: 0, stock: 100, isNew: true },
 ];
