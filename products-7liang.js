abac6e0b4141d8eb3a43fb525be062abc8c17558
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-26 12:20:59 +0800

    Update products-7liang.js

--- products-7liang.js
+++ products-7liang.js
@@ -1,1 +1,7 @@
 
+// 粮油干货 (catId: 6)
+window.PRODUCTS_CAT_6 = [
+  { id: 1, catId: 6, subCatId: 602, name: '树番茄汤底 1.2kg (300g*4)', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/7liang/1yj/1.png?raw=true', price: 49.9, oldPrice: 49.9, sold: 0, stock: 100, isNew: true },
+  { id: 2, catId: 6, subCatId: 602, name: '有言有味 酸菜多多酸菜鱼调料 282g*5', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/7liang/1yj/2.png?raw=true', price: 49.9, oldPrice: 49.9, sold: 0, stock: 100, isNew: true },
+  { id: 3, catId: 6, subCatId: 602, name: '周黑鸭 经典卤料包 1.62kg', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/7liang/1yj/3.png?raw=true', price: 65.9, oldPrice: 65.9, sold: 0, stock: 100, isNew: true },
+];
