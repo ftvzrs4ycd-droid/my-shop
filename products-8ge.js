20f81a575cca38a728e6092d5031d4a719d2752d
ftvzrs4ycd-droid <ftvzrs4ycd@privaterelay.appleid.com>
2026-09-26 14:19:03 +0800

    Update products-8ge.js

--- products-8ge.js
+++ products-8ge.js
@@ -1,1 +1,7 @@
 
+// 个护美妆 (catId: 7)
+window.PRODUCTS_CAT_7 = [
+  { id: 1, catId: 7, subCatId: 702, name: '丝塔芙 舒润保湿霜套组453g*2', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/8ge/1qd/1.png?raw=true', price: 216.9, oldPrice: 216.9, sold: 0, stock: 100, isNew: true },
+  { id: 2, catId: 7, subCatId: 702, name: 'DHC 日本进口橄榄护唇膏1.5g*4 支', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/8ge/1qd/2.png?raw=true', price: 148.9, oldPrice: 148.9, sold: 0, stock: 100, isNew: true },
+  { id: 3, catId: 7, subCatId: 702, name: '碧柔 日本进口 悬挂式身体乳 285mL*2', img: 'https://cdn.jsdelivr.net/gh/ftvzrs4ycd-droid/my-shop@main/images/8ge/1qd/3.png?raw=true', price: 89.9, oldPrice: 89.9, sold: 0, stock: 100, isNew: true },
+];
